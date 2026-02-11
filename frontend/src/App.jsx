0d6f9834f6303c4c339d14befe6923b4c2eca553
sanafathima-00 <sanafathima2312@gmail.com>
2026-02-11 20:42:54 +0530

    Bangalore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import Categories from "./components/Categories";
+import CityPage from "./pages/CityPage";
 
 function App() {
   return (
-    <div>
-      <Hero />
-      <Categories />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Hero />
+            <Categories />
+          </>
+        }
+      />
+
+      <Route path="/city/:name" element={<CityPage />} />
+    </Routes>
   );
 }
 
--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,16 +1,42 @@
 import "./Categories.css";
+import { useNavigate } from "react-router-dom";
+
+import bangalore from '../assets/Bangalore/Vidhana-Soudha_BLR.jpg';
+import chennai from '../assets/Chennai/Church_CHE.jpg';
+import hyderabad from '../assets/Hyderabad/Char-Minar_HYD.jpg';
+import delhi from '../assets/Delhi/Red-Fort_DEL.jpg';
+import mumbai from '../assets/Mumbai/Gateway-of-India_MUM.jpg';
 
 function Categories() {
+  const navigate = useNavigate();
+
+  const cities = [
+    { name: "Bangalore", image: bangalore },
+    { name: "Chennai", image: chennai },
+    { name: "Hyderabad", image: hyderabad },
+    { name: "Delhi", image: delhi },
+    { name: "Mumbai", image: mumbai },
+  ];
+
+  const handleClick = (cityName) => {
+    navigate(`/city/${cityName.toLowerCase()}`);
+  };
+
   return (
     <section className="categories">
       <h2>Popular cities</h2>
 
       <div className="category-list">
-        <div className="category-card">Bangalore</div>
-        <div className="category-card">Chennai</div>
-        <div className="category-card">Hyderabad</div>
-        <div className="category-card">Delhi</div>
-        <div className="category-card">Mumbai</div>
+        {cities.map((city, index) => (
+          <div
+            className="category-card"
+            key={index}
+            onClick={() => handleClick(city.name)}
+          >
+            <img src={city.image} alt={city.name} />
+            <p>{city.name}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
